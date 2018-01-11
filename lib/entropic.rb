da3015678c716876e119aadffb7ed1032c558546
Will Fitzgerald <willf@github.com>
2018-01-11 17:28:14 -0500

    initial real code commit

--- lib/entropic.rb
+++ lib/entropic.rb
@@ -1,5 +1,230 @@
 require "entropic/version"
 
+# Public: classes and methods useful for estimating entropy on strings.
+#
+# Examples
+#
+#   model = Entropic.Model.read("ngrams.tsv")
+#   model.predict("the")
+#   # => { log_prob_total: -101.1, log_prob_average: -20.02, size: 5 }
+#
 module Entropic
-  # Your code goes here...
+
+  # Public: create a sliding window of ngrams from a string
+  #
+  # string: The String to slide over
+  # n: The Integer defining the size of the ngrams
+  #
+  # Examples
+  #
+  #  sliding('01234', 2)  
+  #  # => ['01', '12', '23', '34']
+  #
+  def sliding(string, n)
+    (0..string.length - n).map { |i| (string[i, n]).to_s }
+  end
+
+  # Public: a counter for ngrams
+  class NGramCounter
+    attr_accessor :size, :counts, :total
+    def initialize(size)
+      @size = size
+      @counts = Hash.new(0)
+      @total = 0
+    end
+
+    # Public: update a counter with a string, and a multiplier
+    #
+    # Examples
+    # 
+    # counter = NGramCounter.new(2)
+    # counter.update_with_multiplier('01234', 1)
+    #
+    # string: The String to update with
+    # multiplier: The Integer describing how much weight (will often be 1)
+    #
+    def update_with_multiplier(string, multiplier)
+      sliding(string, @size).each do |ngram|
+        @counts[ngram] += multiplier
+        @total += multiplier
+      end
+    end
+
+    # Public: update a counter with a string, with a multiplier of 1
+    #
+    # Examples
+    # 
+    # counter = NGramCounter.new(2)
+    # counter.update('01234')
+    #
+    # string: The String to update with
+    #
+    def update(string)
+      update_with_multiplier(string, 1)
+    end
+ 
+    # Public: get count for string, with default
+    #
+    # Examples
+    # 
+    # counter = NGramCounter.new(2)
+    # counter.update('01234')
+    # counter.count('01', 0)
+    # #=> 1
+    # counter.count('bob, 0)
+    # #=> 0
+    #
+    # ngram: The String to check
+    # if_not_found : what to update with
+    #
+    def count(ngram, if_not_found)
+      @counts.fetch(ngram, if_not_found)
+    end
+  end
+
+  # Public; A model for entropy
+  class Model
+    VERSION = '1.0.0'.freeze
+    attr_accessor :size, :map
+
+    def initialize(size)
+      @size = size
+      @map = {}
+      (1..size).each { |key| @map[key] = NGramCounter.new(key) }
+    end
+
+    # Public: update a model with a string, and a multiplier
+    #
+    # Examples
+    # 
+    # model = Model.new(2)
+    # model.update_with_multiplier('01234', 1)
+    #
+    # string: The String to update with
+    # multiplier: The Integer describing how much weight (will often be 1)
+    #
+    def update_with_multiplier(string, multiplier)
+      @map.each do |_, counter|
+        counter.update_with_multiplier(string, multiplier)
+      end
+    end
+
+    # Public: update a model with a string, with mulitplier or 1
+    #
+    # Examples
+    # 
+    # model = Model.new(2)
+    # model.update('01234')
+    #
+    # string: The String to update with
+    #
+    def update(string)
+      update_with_multiplier(string, 1)
+    end
+
+    # Public: log probability of a ngram string in a model
+    # returns value of first suffix of string
+    # or log_prob of a 1-gram appearing once if no suffix found
+    #
+    # Examples
+    # 
+    # model = Model.new(2)
+    # model.update('01234')
+    # model.log_prob('01')
+    # 
+    # string: The String to query
+    #
+    def log_prob(key)
+      last_total = 1
+      if @map.all? { |_, m| m.counts.empty? } || !key || key == ''
+        return Math.log(0, 2.0) # -Infinity
+      end
+
+      (1..key.size).each do |i|
+        k = key[-i..-1]
+        counter = @map.fetch(k.size, nil)
+        next unless counter
+        count = counter.counts.fetch(k, nil)
+        return Math.log(count, 2.0) - Math.log(counter.total, 2.0) if count
+        last_total = counter.total
+      end
+      # found it nowhere. Return '1 count' from last total
+      Math.log(1.0, 2.0) - Math.log(last_total, 2.0)
+    end
+
+    # Public: dump model to some io object
+    # 
+    # io: the IOWriter to write to
+    #
+    def dump(io)
+      @map.each do |k, m|
+        m.counts.each do |ngram, count|
+          io.write("#{k}\t#{ngram}\t#{count}\n")
+        end
+      end
+    end
+
+    # Public: predict the log_prob sum and average over a string
+    #         which will be split into ngrams
+    # 
+    # string: The String to query
+    # 
+    # returns: a dictionary of
+    #  - log_prob_total
+    #  - log_prob_average
+    #  - size (number of ngrams in string)
+    def predict(string)
+      ngrams = sliding(string, @size)
+      log_prob_total = ngrams.map { |ngram| log_prob(ngram) }.inject(0.0, :+)
+      log_prob_average = log_prob_total / ngrams.size.to_f
+      { log_prob_total: log_prob_total, log_prob_average: log_prob_average, size: ngrams.size }
+    end
+
+    # Public: create a Model from reading from an IO object
+    #
+    # io: the IOReader
+    #
+    # returns: Model with stats filled in, and size of largest ngram
+    def self.read(io)
+      model = Model.new(0)
+      max_size = 0
+      io.each_line do |string|
+        ngram_size, ngram, count = string.strip.split(/\t/)
+        ngram_size = ngram_size.to_i
+        count = count.to_f
+        model.map[ngram_size] = NGramCounter.new(ngram_size) unless model.map.include?(ngram_size)
+        counter = model.map[ngram_size]
+        counter.total += count
+        counter.counts[ngram] = count
+        max_size = ngram_size if ngram_size > max_size
+      end
+      model.size = max_size
+      model
+    end
+
+    # Public: Train a model on a bunch of data, line by line
+    #
+    # io: the IOReader
+    # 
+    def train(io)
+      io.each_line do |string|
+        update(string)
+      end
+    end
+
+    # Public: Train a model on a bunch of data, line by line,
+    #         with a multiplier
+    #         each data line should be <string><tab><multiplier>
+    #
+    # io: the IOReader
+    # 
+    def train_with_multiplier(io)
+      io.each_line do |string|
+        text, count = string.strip.split(/\t/)
+        count = count.to_i
+        update_with_multiplier(text, count)
+      end
+    end
+  end
 end
+
--- spec/entropic_spec.rb
+++ spec/entropic_spec.rb
@@ -2,10 +2,133 @@ require "spec_helper"
 
 RSpec.describe Entropic do
   it "has a version number" do
-    expect(Entropic::VERSION).not_to be nil
+    Entropic::VERSION.should  be_a_kind_of String
+  end
+end
+
+RSpec.describe NGramCounter do 
+  model = NGramCounter.new(2)
+
+  describe "::initialize" do
+      it "should allow a size to be specified" do
+          model.size.should eq(2)
+      end
+
+      it "should initialize total to 0" do
+          model.total.should eq(0)
+      end
+
+      it "initialize counts to an empty hash" do
+          model.counts.size.should eq(0)
+      end
+
+      it "should update the model when requested" do
+          m = NGramCounter.new(2)
+          m.update('test')
+          m.total.should be >(0.0)
+          m.counts['te'].should be > (0.0)
+      end
+
+      it "should return 1.0 for values not found" do
+          model.count("foo", 1.0).should eq(1.0)
+      end
+  end
+end
+
+RSpec.describe Model do
+  se = Model.new(2)
+
+  it "should have a version" do
+      Model::VERSION.should be_a_kind_of String
   end
 
-  it "does something useful" do
-    expect(false).to eq(true)
+  describe "::initialize" do
+      it "should allow a size to be specified" do
+          se.size.should eq(2)
+      end
+
+      it "should create a model dictionary of size maxsize" do
+          se.map.keys.size.should eq(2)
+      end
+  end
+
+  describe "#sliding" do
+      it "should convert a string into an array of character ngrams" do
+          sliding('test',2).should eq(['te', 'es', 'st'])
+      end
+  end
+
+  describe "#log_prob" do
+      it "should produce -Infinity when untrained" do
+          se.log_prob("test").should eq(Math.log(0, 2.0))
+      end
+
+      it "should produce a result when trained and ngram present" do
+          se = Model.new(2)
+          se.train(StringIO.new("test"))
+          se.log_prob("te").should be < (0.0)
+          se.log_prob("te").should be >  (Math.log(0, 2.0))
+      end
+
+      it "should produce a result when trained and ngram is not present" do
+          se = Model.new(2)
+          se.train(StringIO.new("testtest"))
+          se.log_prob("ZZ").should be < se.log_prob("te")
+          se.log_prob("ZZ").should < (se.log_prob("tt"))
+      end
+
+      it "should be able to read dumped data" do
+          data = <<HERE
+2	fa	13798.000000
+2	PG	13.000000
+2	Os	35.000000
+2	zw	893.000000
+2	6D	7.000000
+2	Hg	1.000000
+2	RU	15.000000
+2	Zg	3.000000
+2	tp	2237.000000
+HERE
+          se0 = Model.read(StringIO.new(data))
+          se0.log_prob("fa").should be < (0.0)
+          se0.log_prob("fa").should be >  (Math.log(0, 2.0))
+      end
+
+      it "should be able to predict" do 
+          se0 = Model.new(2)
+          data = <<HERE
+2	fa	13798.000000
+2	PG	13.000000
+2	Os	35.000000
+2	zw	893.000000
+2	6D	7.000000
+2	Hg	1.000000
+2	RU	15.000000
+2	Zg	3.000000
+2	tp	2237.000000
+HERE
+          se0 = Model.read(StringIO.new(data))
+          se0.size.should eq(2)
+          key = "faPGzw"
+          d = se0.predict(key)
+          d.fetch(:size, 0).should eq(5)
+          d.fetch(:log_prob_total, 0).should be < 0.0
+          d.fetch(:log_prob_average, 0).should be < 0.0
+      end
+  end
+
+  it "should be able to dump and read" do
+      se0 = Model.new(2)
+      se0.train(StringIO.new("test\nbest\n"))
+      o = StringIO.new('',"w")
+      se0.dump(o)
+      o.close()
+      se2 = Model.read(StringIO.new(o.string))
+      key = "faPGzw"
+      ngrams = sliding(key, 2)
+      d = se2.predict(key)
+      d.fetch(:size, 0).should eq(ngrams.size)
+      d.fetch(:log_prob_total, 0).should be < 0.0
+      d.fetch(:log_prob_average, 0).should be < 0.0
   end
 end
